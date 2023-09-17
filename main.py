e3b55f666c9caca6a1cbc54ae2a0ee22496d2b49
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2023-09-17 01:57:50 -0500

    audio recording + save to mongob done

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import sounddevice as sd
+from pymongo import MongoClient
+from scipy.io.wavfile import write
+import wavio as wv
+from gridfs import GridFS
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    uri = "mongodb+srv://amrit:amrit123@cluster0.buro27m.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(uri)
+    db = client['db']
+    fs = GridFS(db)
+    # reports = db['reports']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Sampling frequency
+    freq = 16000
+
+    # Recording duration
+    duration = 10
+
+    # Start recorder with the given values of
+    # duration and sample frequency
+    recording = sd.rec(int(duration * freq), samplerate=freq, channels=1)
+
+    # Record audio for the given number of seconds
+    sd.wait()
+    print("done recording")
+
+    # This will convert the NumPy array to an audio
+    # file with the given sampling frequency
+    write("recording0.wav", freq, recording)
+    print("recording 0 saved")
+
+    # Convert the NumPy array to audio file
+    wv.write("recording1.wav", recording, freq, sampwidth=2)
+
+    print("recording 1 saved")
+
+    # upload the file to mongodb
+    with open("recording1.wav", "rb") as f:
+        encoded = f.read()
+    print("file read")
+
+    fs.put(encoded, filename="recording1.wav")
+    print("file uploaded")
+
+    client.close()
+    print("connection closed")
